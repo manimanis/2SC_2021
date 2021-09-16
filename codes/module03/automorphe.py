dd4745d72b2dcf8bd0d1a408c1def39495c29b3a
Mohamed Anis MANI <manimohamed@gmail.com>
2021-09-16 07:56:53 +0100

    Working on module 3

---
+++ codes/module03/automorphe.py
@@ -0,0 +1,23 @@
+# Todo 1 : Enlever le commentaire # de la ligne suivante
+# n = int("Donner un nombre > 0 ? ")
+
+# Todo 2 : à supprimer
+n = 76
+
+# Todo 3 : calculer le carré de n
+nc = 5776
+
+# Todo 4 : convertir n et nc en chaînes de caractères
+nch = "76"
+ncch = "5776"
+
+# Todo 5 : trouver la longueur de nch
+l = 2
+
+# Todo 6 : utiliser l pour retrouver les l derniers caractères de ncch
+dcar = "76"
+
+# Todo 7 : déterminer si n est automorphe
+automorphe = True
+
+print(n, "est automorphe ?", automorphe)
