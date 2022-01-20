bd6a44577ad5dccbe32c6a208bc4fec7652e26fb
Mohamed Anis MANI <manimohamed@gmail.com>
2022-01-20 09:17:08 +0100

    mise à jour dc02

--- codes/dc02_2022/vibrations.py
+++ codes/dc02_2022/vibrations.py
@@ -1,23 +1,41 @@
 from random import randint
 
+nm = int(input("Nombre de mesures ? "))
+
 seq = ""
-for i in range(randint(6, 20)):
+for i in range(nm):
     seq = seq + chr(48 + randint(0, 1))
 print(seq)
 
-tm = len(seq)
+for i in range(randint(1, nm // 3 + (nm % 3 != 0))):
+    pos = randint(0, nm-1)
+    seq = seq[:pos] + "2" + seq[pos+1:]
+print(seq)
+
+nmi = 0
+for i in range(nm):
+    if seq[i] == "2":
+        nmi = nmi + 1
+print("Nombre de mesures incorrectes :", nmi)
+
+for i in range(nm-1, -1, -1):
+    if seq[i] == "2":
+        seq = seq[:i] + seq[i+1:]
+print(seq)
+nm = len(seq)
+
 nv = 0
-for i in range(tm):
+for i in range(nm):
     if seq[i] == "0":
         nv = nv + 1
-pv = nv * 100 / tm
-print("Taille du message :", tm)
+pv = nv * 100 / nm
+print("Nombre de mesures :", nm)
 print("Nombre de vibrations :", nv)
 print("Pourcentage de vibrations :", round(pv, 2), "%")
 
-if pv >= 65:
+if pv >= 60:
     print("Le roulement doit être remplacé")
-elif pv >= 55:
+elif pv >= 50:
     print("Le roulement est usé")
 else:
     print("Le roulement est en bon état")
