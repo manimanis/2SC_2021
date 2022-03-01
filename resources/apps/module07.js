ba94e42b560ea32225279dd88d85a18143f71d4f
Mohamed Anis MANI <manimohamed@gmail.com>
2022-03-01 16:36:31 +0100

    mise a jour module 7

---
+++ resources/apps/module07.js
@@ -0,0 +1,52 @@
+const app = new Vue({
+  el: "#anniv",
+  data: {
+    bonbons: 30,
+    mnb: 30,
+    mna: 8,
+    boxes: []
+  },
+  computed: {
+    nb: {
+      get: function () { return this.mnb; },
+      set: function (newVal) {
+        this.mnb = +newVal;
+        this.bonbons = this.mnb;
+        this.boxes = new Array(this.mna).fill(0);
+      }
+    },
+    na: {
+      get: function () { return this.mna; },
+      set: function (newVal) {
+        this.mna = +newVal;
+        this.bonbons = this.mnb;
+        this.boxes = new Array(this.mna).fill(0);
+      }
+    }
+  },
+  mounted: function () {
+    this.reinitialiser();
+  },
+  methods: {
+    mettreBonbon: function () {
+      if (this.bonbons > 0) {
+        const nbh = Math.floor(Math.random() * this.na);
+        this.boxes[nbh]++;
+        this.bonbons--;
+        this.$forceUpdate();
+      }
+    },
+    distribuerBonbons: function () {
+      while (this.bonbons > 0) {
+        const nbh = Math.floor(Math.random() * this.na);
+        this.boxes[nbh]++;
+        this.bonbons--;
+      }
+      this.$forceUpdate();
+    },
+    reinitialiser: function() {
+      this.na = this.na;
+      this.bonbons = this.nb;
+    }
+  }
+});
